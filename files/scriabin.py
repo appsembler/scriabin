b79eefceda7df5087c511646bc5d2ac0bd8ce0f5
Anders Pearson <anders@thraxil.org>
2021-03-16 14:34:14 +0000

    remove pushgateway functionality
    node_exporter and textfile collector is now the preferred approach.

--- files/scriabin.py
+++ files/scriabin.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 from dateutil import parser, tz
 from pygtail import Pygtail
 
-from prometheus_client import CollectorRegistry, push_to_gateway, write_to_textfile
+from prometheus_client import CollectorRegistry, write_to_textfile
 from prometheus_client import Gauge, Histogram
 
 # gather up environment variables
@@ -17,8 +17,6 @@ SLOW_THRESHOLD = float(os.environ.get("SCRIABIN_SLOW_THRESHOLD",
                                       "0.4"))  # seconds
 PROJECT = os.environ["SCRIABIN_PROJECT"]
 APP = os.environ["SCRIABIN_APP"]
-PUSHGATEWAY = os.environ.get("SCRIABIN_PUSHGATEWAY",
-                             'pushgateway.infra.appsembler.com:9091')
 SKIP_PATHS = os.environ.get(
     "SCRIABIN_SKIP_PATHS",
     "/static/,/admin/,/hijack/,/favicon.ico").split(",")
@@ -29,31 +27,32 @@ OFFSET_FILE = "/tmp/scriabin-{}-{}.offset".format(PROJECT, APP)
 # prepare Prometheus metrics
 registry = CollectorRegistry()
 requests_counter = Gauge("nginx_requests", "HTTP Requests",
-                         ["status"],
+                         ["app", "status"],
                          registry=registry)
 # make sure a few are always set, rather than have missing values
-requests_counter.labels(status=200).set(0)
-requests_counter.labels(status=201).set(0)
-requests_counter.labels(status=204).set(0)
-requests_counter.labels(status=206).set(0)
-requests_counter.labels(status=301).set(0)
-requests_counter.labels(status=302).set(0)
-requests_counter.labels(status=304).set(0)
-requests_counter.labels(status=400).set(0)
-requests_counter.labels(status=403).set(0)
-requests_counter.labels(status=404).set(0)
-requests_counter.labels(status=499).set(0)
-requests_counter.labels(status=500).set(0)
-requests_counter.labels(status=502).set(0)
+requests_counter.labels(app=APP, status=200).set(0)
+requests_counter.labels(app=APP, status=201).set(0)
+requests_counter.labels(app=APP, status=204).set(0)
+requests_counter.labels(app=APP, status=206).set(0)
+requests_counter.labels(app=APP, status=301).set(0)
+requests_counter.labels(app=APP, status=302).set(0)
+requests_counter.labels(app=APP, status=304).set(0)
+requests_counter.labels(app=APP, status=400).set(0)
+requests_counter.labels(app=APP, status=403).set(0)
+requests_counter.labels(app=APP, status=404).set(0)
+requests_counter.labels(app=APP, status=499).set(0)
+requests_counter.labels(app=APP, status=500).set(0)
+requests_counter.labels(app=APP, status=502).set(0)
 
 response_time = Histogram("nginx_response_time", "HTTP Response Time (s)",
+                          ["app"],
                           buckets=[.01, 0.02, 0.04, 0.1, 0.2, 0.4, 1.0,
                                    2.0, 4.0, 10.0, 20.0, 40.0,
                                    float("inf")],
                           registry=registry)
 
-apdex_score = Gauge("apdex_score", "Apdex", registry=registry)
-parse_errors = Gauge("parse_errors", "Nginx log parse errors",
+apdex_score = Gauge("apdex_score", "Apdex", ["app"], registry=registry)
+parse_errors = Gauge("parse_errors", "Nginx log parse errors", ["app"],
                      registry=registry)
 
 
@@ -89,28 +88,28 @@ def main():
     entries, errors = log_entries()
     for d in entries:
         total_requests += 1
-        requests_counter.labels(status=d['status']).inc()
+        requests_counter.labels(app=APP, status=d['status']).inc()
         if d['status'].startswith('5'):
             failed_requests += 1
         rt = float(d['request_time'])
-        response_time.observe(rt)
+        response_time.labels(app=APP).observe(rt)
         if rt > SLOW_THRESHOLD:
             slow_requests += 1
 
-    apdex_score.set(1)
+    apdex_score.labels(app=APP).set(1)
     if total_requests > 0:
         satisfied = total_requests - (failed_requests + slow_requests)
-        apdex_score.set((satisfied + (slow_requests / 2)) / total_requests)
+        apdex_score.labels(
+            app=APP).set((satisfied + (slow_requests / 2)) / total_requests)
 
-    parse_errors.set(errors)
+    parse_errors.labels(app=APP).set(errors)
 
     print("total_requests:", total_requests)
     print("failed:", failed_requests)
     print("slow:", slow_requests)
 
-    push_to_gateway(PUSHGATEWAY, job="{}_{}_nginx".format(PROJECT, APP),
-                    registry=registry, grouping_key={'instance': INSTANCE})
-    write_to_textfile("/var/lib/node_exporter/nginx.prom", registry)
+    write_to_textfile(
+        "/var/lib/node_exporter/nginx_{}.prom".format(APP), registry)
 
 
 if __name__ == "__main__":
